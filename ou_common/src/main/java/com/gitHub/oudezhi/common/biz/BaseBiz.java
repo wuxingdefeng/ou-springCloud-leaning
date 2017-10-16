168acad1bf6a1d8360fb22e92135e6caa94b1ec1
oudezhi <ouyang_riguang@163.com>
2017-10-16 17:25:42 +0800

    提交

--- ou_common/src/main/java/com/gitHub/oudezhi/common/biz/BaseBiz.java
+++ ou_common/src/main/java/com/gitHub/oudezhi/common/biz/BaseBiz.java
@@ -1,10 +1,17 @@
 package com.gitHub.oudezhi.common.biz;
 
+import com.gitHub.oudezhi.common.msg.TableResultResponse;
 import com.gitHub.oudezhi.common.util.EntityUtils;
+import com.gitHub.oudezhi.common.util.Query;
+import com.github.pagehelper.Page;
+import com.github.pagehelper.PageHelper;
 import org.springframework.beans.factory.annotation.Autowired;
 import tk.mybatis.mapper.common.Mapper;
+import tk.mybatis.mapper.entity.Example;
 
+import java.lang.reflect.ParameterizedType;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by Administrator on 2017/10/16.
@@ -82,5 +89,18 @@ public abstract class BaseBiz<M extends Mapper<T>,T>{
 
     public int selectCountByExample(Object example) {
         return mapper.selectCountByExample(example);
+    }
+
+    public TableResultResponse<T> selectByQuery(Query query) {
+        Class<T> clazz = (Class<T>) ((ParameterizedType) getClass().getGenericSuperclass()).getActualTypeArguments()[1];
+        Example example = new Example(clazz);
+
+        Example.Criteria criteria = example.createCriteria();
+        for (Map.Entry<String, Object> entry : query.entrySet()) {
+            criteria.andLike(entry.getKey(), "%" + entry.getValue().toString() + "%");
+        }
+        Page<Object> result = PageHelper.startPage(query.getPage(), query.getLimit());
+        List<T> list = mapper.selectByExample(example);
+        return new TableResultResponse<T>(result.getTotal(), list);
     }
 }
